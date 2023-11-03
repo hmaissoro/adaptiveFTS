55837542a9f5472b27691c86b79bbc3e2668451b
MAISSORO Hassan <hmaissoro@clust-n11.domensai.ecole>
2023-11-03 15:15:30 +0100

    estimation on new designs

--- inst/12_mc_simulate_data/12_mc_simu_estimate_mean.R
+++ inst/12_mc_simulate_data/12_mc_simu_estimate_mean.R
@@ -21,9 +21,9 @@ estim_mean_risk_fun <- function(N = 400, lambda = 300, process = "FAR", white_no
   dt_locreg <- readRDS(locreg_file_name)
 
   # Define bandwidth grid
-  K <- 100
-  b0 <- 2 / lambda
-  bK <- lambda ** (- 1 / 3)
+  K <- 20
+  b0 <- 4 * (N * lambda) ** (- 0.9)
+  bK <- 4 * (N * lambda) ** (- 1 / 3)
   a <- exp((log(bK) - log(b0)) / K)
   bw_grid <- b0 * a ** (seq_len(K))
   rm(K, b0, bK, a) ; gc()
@@ -37,8 +37,166 @@ estim_mean_risk_fun <- function(N = 400, lambda = 300, process = "FAR", white_no
       dt_random_mc <- dt_random[id_mc == mc_i]
 
       # Estimate pseudo-true mean function risk
+      dt_risk_mutilde <- data.table::rbindlist(lapply(bw_grid, function(hi, data_common_mc, data_random_mc, bw_grid, t0){
+        dt_Xhat <- data.table::rbindlist(lapply(data_random_mc[, unique(id_curve)], function(curve_index, t0, hi, data){
+          Tn <- data[id_curve == curve_index, tobs]
+          Yn <- data[id_curve == curve_index, X_plus_mean]
+          Xhat <- estimate_nw(y = Yn, t = Tn, tnew = t0, h = hi, smooth_ker = epanechnikov)
+          return(data.table("curve_index" = curve_index, "t" = t0, "h" = hi, "Xhat" = Xhat$yhat))
+        }, data = data_random_mc, t0 = t0, hi = hi))
+
+        # Estimate mean function and add true meanfunction
+        dt_muhat <- dt_Xhat[!is.nan(Xhat), .("muhat" = mean(Xhat)), by = c("t", "h")]
+        dt_muhat <- data.table::merge.data.table(
+          x = dt_muhat,
+          y = data_common_mc[, .("mutilde" = mean(X_plus_mean)), by = "tobs"],
+          by.x = "t", by.y = "tobs")
+        dt_muhat[, mutitle_mse := (muhat - mutilde) ** 2, by = t]
+
+      }, data_common_mc = dt_common_mc, data_random_mc = dt_random_mc, bw_grid = bw_grid, t0 = t0))
+
+      # Estimate the risk of the mean function
+      bw <- unique(dt_random_mc[, .(id_curve, presmooth_bw_plus_mean)])[, presmooth_bw_plus_mean]
+      Ht <- dt_locreg[id_mc == mc_i & order(t), Ht]
+      Lt <- dt_locreg[id_mc == mc_i & order(t), Lt]
+      Ht_plus_mean <- dt_locreg[id_mc == mc_i & order(t), Ht_plus_mean]
+      Lt_plus_mean <- dt_locreg[id_mc == mc_i & order(t), Lt_plus_mean]
+
+      dt_risk_muhat <- estimate_mean_risk(
+        data = dt_random_mc, idcol = "id_curve", tcol = 'tobs', ycol = "X_plus_mean",
+        t = t0, bw_grid = bw_grid, Ht = Ht, Lt = Lt, Delta = NULL, h = bw
+      )
+
+      dt_risk_muhat_plus_mean <- estimate_mean_risk(
+        data = dt_random_mc, idcol = "id_curve", tcol = 'tobs', ycol = "X_plus_mean",
+        t = t0, bw_grid = bw_grid, Ht = Ht_plus_mean, Lt = Lt_plus_mean, Delta = NULL, h = bw
+      )
+      data.table::setnames(x = dt_risk_muhat_plus_mean,
+                           old = c("Ht", "Lt", "locreg_bw", "bias_term", "varriance_term", "dependence_term", "mean_risk"),
+                           new = c("Ht_plus_mean", "Lt_plus_mean", "locreg_bw_plus_mean", "bias_term_plus_mean",
+                                   "varriance_term_plus_mean", "dependence_term_plus_mean", "mean_risk_plus_mean"))
+
+      ## Merge the mean risk estimates and return the obtained result
+      dt_risk_muhat_res <- data.table::merge.data.table(
+        x = dt_risk_muhat,
+        y = dt_risk_muhat_plus_mean,
+        by = c("t", "h")
+      )
+      rm(dt_risk_muhat, dt_risk_muhat_plus_mean) ; gc()
+
+      ## Add the MSE of mutilde
+      dt_risk <- data.table::merge.data.table(
+        x = dt_risk_muhat_res, y = dt_risk_mutilde, by = c("t", "h")
+      )
+      dt_res <- data.table::data.table("id_mc" = mc_i, "N" = Ni, "lambda" = lambdai, dt_risk)
+      rm(dt_risk_muhat_res, dt_risk_mutilde, dt_risk) ; gc()
+      return(dt_res)
+    }, mc.cores = 75, dt_random = dt_random, dt_common = dt_common, dt_locreg = dt_locreg, Ni = N, lambdai = lambda, bw_grid = bw_grid, t0 = t0))
+
+  } else if (white_noise == "fBm") {
+    # Estimate local regularity by mc
+    dt_risk_mc <- data.table::rbindlist(parallel::mclapply(index_mc, function(mc_i, dt_random, dt_common, dt_locreg, Ni, lambdai, bw_grid, t0){
+      # Extract and sort data
+      dt_common_mc <- dt_common[id_mc == mc_i]
+      dt_random_mc <- dt_random[id_mc == mc_i]
+      Hvec <- dt_random_mc[, sort(unique(Htrue))]
+
+      dt_by_Hvec <- data.table::rbindlist(lapply(Hvec, function(Hi, dt_common_mc, dt_random_mc, dt_locreg, Ni, lambdai, bw_grid, t0){
+        # Estimate pseudo-true mean function risk
+        dt_risk_mutilde <- data.table::rbindlist(lapply(bw_grid, function(hi, data_common_mc, data_random_mc, bw_grid, t0){
+          dt_Xhat <- data.table::rbindlist(lapply(data_random_mc[, unique(id_curve)], function(curve_index, t0, hi, data){
+            Tn <- data[id_curve == curve_index, tobs]
+            Yn <- data[id_curve == curve_index, X_plus_mean]
+            Xhat <- estimate_nw(y = Yn, t = Tn, tnew = t0, h = hi, smooth_ker = epanechnikov)
+            return(data.table("curve_index" = curve_index, "t" = t0, "h" = hi, "Xhat" = Xhat$yhat))
+          }, data = data_random_mc, t0 = t0, hi = hi))
+
+          # Estimate mean function and add true meanfunction
+          dt_muhat <- dt_Xhat[!is.nan(Xhat), .("muhat" = mean(Xhat)), by = c("t", "h")]
+          dt_muhat <- data.table::merge.data.table(
+            x = dt_muhat,
+            y = data_common_mc[, .("mutilde" = mean(X_plus_mean)), by = "tobs"],
+            by.x = "t", by.y = "tobs")
+          dt_muhat[, mutitle_mse := (muhat - mutilde) ** 2, by = t]
+
+        }, data_common_mc = dt_common_mc[Htrue == Hi], data_random_mc = dt_random_mc[Htrue == Hi], bw_grid = bw_grid, t0 = t0))
+
+        # Estimate the risk of the mean function
+        bw <- unique(dt_random_mc[Htrue == Hi][, .(id_curve, presmooth_bw_plus_mean)])[, presmooth_bw_plus_mean]
+        Ht <- dt_locreg[Htrue == Hi][id_mc == mc_i & order(t), Ht]
+        Lt <- dt_locreg[Htrue == Hi][id_mc == mc_i & order(t), Lt]
+        Ht_plus_mean <- dt_locreg[Htrue == Hi][id_mc == mc_i & order(t), Ht_plus_mean]
+        Lt_plus_mean <- dt_locreg[Htrue == Hi][id_mc == mc_i & order(t), Lt_plus_mean]
+
+        dt_risk_muhat <- estimate_mean_risk(
+          data = dt_random_mc[Htrue == Hi], idcol = "id_curve", tcol = 'tobs', ycol = "X_plus_mean",
+          t = t0, bw_grid = bw_grid, Ht = Ht, Lt = Lt, Delta = NULL, h = bw
+        )
+
+        dt_risk_muhat_plus_mean <- estimate_mean_risk(
+          data = dt_random_mc[Htrue == Hi], idcol = "id_curve", tcol = 'tobs', ycol = "X_plus_mean",
+          t = t0, bw_grid = bw_grid, Ht = Ht_plus_mean, Lt = Lt_plus_mean, Delta = NULL, h = bw
+        )
+        data.table::setnames(x = dt_risk_muhat_plus_mean,
+                             old = c("Ht", "Lt", "locreg_bw", "bias_term", "varriance_term", "dependence_term", "mean_risk"),
+                             new = c("Ht_plus_mean", "Lt_plus_mean", "locreg_bw_plus_mean", "bias_term_plus_mean",
+                                     "varriance_term_plus_mean", "dependence_term_plus_mean", "mean_risk_plus_mean"))
+        ## Merge the mean risk estimates and return the obtained result
+        dt_risk_muhat_res <- data.table::merge.data.table(
+          x = dt_risk_muhat,
+          y = dt_risk_muhat_plus_mean,
+          by = c("t", "h")
+        )
+        rm(dt_risk_muhat, dt_risk_muhat_plus_mean) ; gc()
+
+        ## Add the MSE of mutilde and return
+        dt_risk <- data.table::merge.data.table(
+          x = dt_risk_muhat_res, y = dt_risk_mutilde, by = c("t", "h")
+        )
+        dt_res <- data.table::data.table("id_mc" = mc_i, "N" = Ni, "lambda" = lambdai, "Htrue" = Hi, dt_risk)
+        rm(dt_risk_muhat_res, dt_risk_mutilde, dt_risk) ; gc()
+        return(dt_res)
+      }, dt_common_mc = dt_common_mc, dt_random_mc = dt_random_mc, dt_locreg = dt_locreg, Ni = N, lambdai = lambda, bw_grid = bw_grid, t0 = t0))
+
+      return(dt_by_Hvec)
+    }, mc.cores = 75, dt_random = dt_random, dt_common = dt_common, dt_locreg = dt_locreg, Ni = N, lambdai = lambda, bw_grid = bw_grid, t0 = t0))
+  }
+
+  ## Save
+  file_name <- paste0("./inst/12_mc_simulate_data/", process, "/mean_estimates/dt_mean_risk_",
+                      process,"_", white_noise, "_", "N=", N, "_lambda=", lambda, "_", design,".RDS")
+  saveRDS(object = dt_risk_mc, file = file_name)
+  rm(data_file_name, locreg_file_name, file_name, dt_risk_mc) ; gc() ; gc()
+
+  return(paste0("Done : dt_mean_risk_", process,"_", white_noise, "_", "N=", N, "_lambda=", lambda, "_", design,".RDS at ", Sys.time()))
+}
+
+## Estimate mean function
+estim_mean_fun <- function(N = 400, lambda = 300, process = "FAR", white_noise = "mfBm", design = "d1", t0 = t0){
+  # Load mean risk data
+  file_name <- paste0("./inst/12_mc_simulate_data/", process, "/mean_estimates/dt_mean_risk_",
+                      process,"_", white_noise, "_", "N=", N, "_lambda=", lambda, "_", design,".RDS")
+  dt_mean_risk <- readRDS(file_name)
+  dt_optbw <- dt_mean_risk[, .("optbw" = h[which.min(mean_risk)],
+                               "optbw_plus_mean" = h[which.min(mean_risk_plus_mean)]),
+                           by = c("id_mc", "t")]
+  # Load raw data
+  data_file_name <- paste0("./inst/12_mc_simulate_data/", process, "/data/dt_mc_",
+                           process,"_", white_noise, "_", "N=", N, "_lambda=", lambda, "_", design,".RDS")
+  dt <- readRDS(data_file_name)
+  dt <- dt[ttag == "trandom"]
+  index_mc <- dt[, unique(id_mc)]
+
+  if (white_noise == "mfBm") {
+
+    # Estimate local regularity by mc
+    dt_mean_mc <- data.table::rbindlist(parallel::mclapply(index_mc, function(mc_i, data, dt_optbw, Ni, lambdai, bw_grid, t0){
+      # Extract data
+      dt_random_mc <- data[id_mc == mc_i]
+
+      # Estimate pseudo-true mean function risk
       dt_risk_mutilde <- data.table::rbindlist(lapply(bw_grid, function(hi, data, bw_grid, t0){
-        dt_Xhat <- data.table::rbindlist(lapply(dt_common_mc[, unique(id_curve)], function(curve_index, t0, hi, data){
+        dt_Xhat <- data.table::rbindlist(lapply(data[, unique(id_curve)], function(curve_index, t0, hi, data){
           Tn <- data[id_curve == curve_index, tobs]
           Yn <- data[id_curve == curve_index, X_plus_mean]
           Xhat <- estimate_nw(y = Yn, t = Tn, tnew = t0, h = hi, smooth_ker = epanechnikov)
@@ -91,7 +249,7 @@ estim_mean_risk_fun <- function(N = 400, lambda = 300, process = "FAR", white_no
       dt_res <- data.table::data.table("id_mc" = mc_i, "N" = Ni, "lambda" = lambdai, dt_risk)
       rm(dt_risk_muhat_res, dt_risk_mutilde, dt_risk) ; gc()
       return(dt_res)
-    }, mc.cores = 75, dt_random = dt_random, dt_common = dt_common, dt_locreg = dt_locreg, Ni = N, lambdai = lambda, bw_grid = bw_grid, t0 = t0))
+    }, mc.cores = 75, data = dt, dt_optbw = dt_optbw, dt_locreg = dt_locreg, Ni = N, lambdai = lambda, bw_grid = bw_grid, t0 = t0))
 
   } else if (white_noise == "fBm") {
     # Estimate local regularity by mc
@@ -104,7 +262,7 @@ estim_mean_risk_fun <- function(N = 400, lambda = 300, process = "FAR", white_no
       dt_by_Hvec <- data.table::rbindlist(lapply(Hvec, function(Hi, dt_common_mc, dt_random_mc, dt_locreg, Ni, lambdai, bw_grid, t0){
         # Estimate pseudo-true mean function risk
         dt_risk_mutilde <- data.table::rbindlist(lapply(bw_grid, function(hi, data, bw_grid, t0){
-          dt_Xhat <- data.table::rbindlist(lapply(dt_common_mc[, unique(id_curve)], function(curve_index, t0, hi, data){
+          dt_Xhat <- data.table::rbindlist(lapply(data[, unique(id_curve)], function(curve_index, t0, hi, data){
             Tn <- data[id_curve == curve_index, tobs]
             Yn <- data[id_curve == curve_index, X_plus_mean]
             Xhat <- estimate_nw(y = Yn, t = Tn, tnew = t0, h = hi, smooth_ker = epanechnikov)
@@ -169,11 +327,7 @@ estim_mean_risk_fun <- function(N = 400, lambda = 300, process = "FAR", white_no
   rm(data_file_name, locreg_file_name, file_name, dt_risk_mc) ; gc() ; gc()
 
   return(paste0("Done : dt_mean_risk_", process,"_", white_noise, "_", "N=", N, "_lambda=", lambda, "_", design,".RDS"))
-}
 
-## Estimate mean function
-estim_mean_fun <- function(N = 400, lambda = 300, process = "FAR", white_noise = "mfBm", design = "d1", t0 = t0){
-  # comming ...
 }
 
 # Estimate mean risk function ----
---
+++ inst/12_mc_simulate_data/12_mc_simu_estimate_mean_graph.R.R
@@ -0,0 +1,292 @@
+library(data.table)
+library(magrittr)
+library(ggplot2)
+library(gridExtra)
+library(latex2exp)
+
+# Simulation global parameters----
+sig <- 0.25
+mc <- 100
+t0 <- c(0.2, 0.4, 0.7, 0.8)
+
+## Logistic constant hurst function
+Hlogistic <- function(t){
+  hurst_logistic(t, h_left = 0.4, h_right = 0.6,
+                 change_point_position = 0.5, slope = 50)
+}
+
+# Local regularity graph function ----
+
+ggplot_mean_risk <- function(N = 400, lambda = 300, process = "FAR", white_noise = "mfBm", design = "d1", param = "Ht"){
+  ## Load data
+  file_name <- paste0("./inst/12_mc_simulate_data/", process, "/mean_estimates/dt_mean_risk_",
+                      process,"_", white_noise, "_", "N=", N, "_lambda=", lambda, "_", design,".RDS")
+  dt_mean_risk <- readRDS(file_name)
+  dt_mean_risk[id_mc == 1 & t == 0.2]
+  dt_risk <- dt_mean_risk[, .("mean_risk" = mean(mean_risk),
+                              "mean_risk_plus_mean" = mean(mean_risk_plus_mean),
+                              "mutitle_mse" = mean(mutitle_mse)),
+                          by = c("t", "h")]
+
+  dt_mean_risk[, .("optbw" = h[which.min(mean_risk)],
+                   "optbw_plus_mean" = h[which.min(mean_risk_plus_mean)],
+                   "optbw_mse" = h[which.min(mutitle_mse)]),
+               by = c("id_mc", "t")]
+  ggplot(data = dt_risk[t == 0.2], mapping = aes(x = h, y = mutitle_mse)) +
+    geom_line()
+
+  ## ggplot parameters
+  geom_theme <- theme_minimal() +
+    theme(plot.title = element_text(size = 9),
+          axis.title = element_text(size = 12),
+          axis.title.x = element_text(size = 12, margin = margin(t = 10, r = 0, b = 0, l = 0)),
+          axis.title.y = element_text(size = 12, margin = margin(t = 10, r = 10, b = 0, l = 0)),
+          axis.text.x =  element_text(size = 12),
+          axis.text.y =  element_text(size = 12),
+          legend.text = element_text(size = 12),
+          legend.title = element_text(size = 12),
+          legend.key.width= unit(0.8, 'cm'),
+          legend.position = "top")
+
+  if (white_noise == "mfBm") {
+    ## define segment and set scale label
+    dt_pr <- unique(dt_locreg[, .("t" = as.factor(t), "x" = as.factor(t - 0.05),
+                                  "xend" = as.factor(t + 0.05), "Htrue" =  Hlogistic(t))])
+    scale_label <- c(dt_pr[, t], dt_pr[, x], dt_pr[, xend])
+    scale_label <- sort(as.character(scale_label))
+    scale_label[- which(scale_label %in% as.character(dt_pr[, t]))] <- ""
+
+    ## set t as factor
+    dt_locreg[, t := as.factor(t)]
+    if (param == "Ht") {
+      title_exp <- paste0("Zero-mean ", process, "(1) - WN = ", white_noise, " - N = ", N , ", $\\lambda$=", lambda)
+      y_lim <- c(0.2, 0.9)
+      x_lab <- "t"
+      y_lab <- latex2exp::TeX("$\\widehat{H}_t$")
+      geom_true_param <- geom_segment(
+        data = dt_pr, mapping = aes(x = x, xend = xend, y = Htrue, yend = Htrue),
+        linetype = 2)
+    } else if (param == "Ht_plus_mean") {
+      title_exp <- paste0(process, "(1) - WN = ", white_noise, " - N = ", N , ", $\\lambda$=", lambda)
+      y_lim <- c(0.2, 0.9)
+      x_lab <- "t"
+      y_lab <-  latex2exp::TeX("$\\widehat{H}_t$")
+      geom_true_param <- geom_segment(
+        data = dt_pr, mapping = aes(x = x, xend = xend, y = Htrue, yend = Htrue),
+        linetype = 2)
+    } else if (param == "Lt") {
+      title_exp <- paste0("Zero-mean ", process, "(1) - WN = ", white_noise, " - N = ", N , ", $\\lambda$=", lambda)
+      y_lim <- c(-2, 15)
+      x_lab <- "t"
+      geom_true_param <- geom_hline(yintercept = 4, color = "#283747", linetype = 2)
+      y_lab <- latex2exp::TeX("$\\widehat{L}_t^2$")
+      scale_label <- scale_label[! scale_label == ""]
+    } else if (param == "Lt_plus_mean") {
+      title_exp <- paste0(process, "(1) - WN = ", white_noise, " - N = ", N , ", $\\lambda$=", lambda)
+      y_lim <- c(-2, 15)
+      x_lab <- "t"
+      y_lab <- latex2exp::TeX("$\\widehat{L}_t^2$")
+      geom_true_param <- geom_hline(yintercept = 4, color = "#283747", linetype = 2)
+      scale_label <- scale_label[! scale_label == ""]
+    }
+    ggplt <- ggplot(data = dt_locreg, mapping = aes(x = t, y = get(param))) +
+      geom_boxplot() +
+      ylim(y_lim) +
+      ggtitle(latex2exp::TeX(title_exp)) +
+      xlab(x_lab) +
+      ylab(y_lab) +
+      geom_true_param +
+      scale_x_discrete(labels = scale_label) +
+      geom_theme
+
+  } else if (white_noise == "fBm") {
+    ## define segment and set scale label
+    dt_pr <- dt_pr <- unique(dt_locreg[, .("t" = as.factor(t), "x" = as.factor(Htrue - 0.05),
+                                           "xend" = as.factor(Htrue + 0.05), Htrue)])
+    scale_label <- c(dt_pr[, as.factor(Htrue)], dt_pr[, x], dt_pr[, xend])
+    scale_label <- sort(as.character(unique(scale_label)))
+    scale_label[- which(scale_label %in% as.character(dt_pr[, as.factor(Htrue)]))] <- ""
+
+    ## set t and Htrue as factor
+    dt_locreg[, t := as.factor(t)]
+    dt_locreg[, Htrue := as.factor(Htrue)]
+
+    if (param == "Ht") {
+      title_exp <- paste0("Zero-mean ", process, "(1) - WN = ", white_noise, " - N = ", N , ", $\\lambda$=", lambda)
+      y_lim <- c(0.2, 0.9)
+      x_lab <- latex2exp::TeX("True $H_t$")
+      y_lab <- latex2exp::TeX("$\\widehat{H}_t$")
+      geom_true_param <- geom_segment(
+        data = dt_pr, mapping = aes(x = x, xend = xend, y = Htrue, yend = Htrue),
+        linetype = 2)
+    } else if (param == "Ht_plus_mean") {
+      title_exp <- paste0(process, "(1) - WN = ", white_noise, " - N = ", N , ", $\\lambda$=", lambda)
+      y_lim <- c(0.2, 0.9)
+      x_lab <- latex2exp::TeX("True $H_t$")
+      y_lab <-  latex2exp::TeX("$\\widehat{H}_t$")
+      geom_true_param <- geom_segment(
+        data = dt_pr, mapping = aes(x = x, xend = xend, y = Htrue, yend = Htrue),
+        linetype = 2)
+    } else if (param == "Lt") {
+      title_exp <- paste0("Zero-mean ", process, "(1) - WN = ", white_noise, " - N = ", N , ", $\\lambda$=", lambda)
+      y_lim <- c(-2, 15)
+      x_lab <- latex2exp::TeX("True $H_t$")
+      geom_true_param <- geom_hline(yintercept = 4, color = "#283747", linetype = 2)
+      y_lab <- latex2exp::TeX("$\\widehat{L}_t^2$")
+      scale_label <- scale_label[! scale_label == ""]
+    } else if (param == "Lt_plus_mean"){
+      title_exp <- paste0(process, "(1) - WN = ", white_noise, " - N = ", N , ", $\\lambda$=", lambda)
+      y_lim <- c(-2, 15)
+      x_lab <- latex2exp::TeX("True $H_t$")
+      y_lab <- latex2exp::TeX("$\\widehat{L}_t^2$")
+      geom_true_param <- geom_hline(yintercept = 4, color = "#283747", linetype = 2)
+      scale_label <- scale_label[! scale_label == ""]
+    }
+
+    ggplt <- ggplot(data = dt_locreg, mapping = aes(x = Htrue, y = get(param), fill = t)) +
+      geom_boxplot() +
+      ylim(y_lim) +
+      ggtitle(latex2exp::TeX(title_exp)) +
+      xlab(x_lab) +
+      ylab(y_lab) +
+      geom_true_param +
+      scale_x_discrete(labels = scale_label) +
+      scale_fill_grey() +
+      geom_theme
+  }
+
+  return(ggplt)
+}
+
+# Plot local regularity parameters ----
+## design 1 ----
+### FAR ----
+g_locreg_far_mfBm_d1 <- gridExtra::grid.arrange(
+  ggplot_locreg(N = 400, lambda = 300, process = "FAR", white_noise = "mfBm", design = "d1", param = "Ht"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FAR", white_noise = "mfBm", design = "d1", param = "Ht_plus_mean"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FAR", white_noise = "mfBm", design = "d1", param = "Lt"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FAR", white_noise = "mfBm", design = "d1", param = "Lt_plus_mean"),
+  nrow = 2, ncol = 2
+)
+ggsave(filename = "./inst/12_mc_simulate_data/graphs/locreg_far_mfBm_d1.png", plot = g_locreg_far_mfBm_d1,
+       width = 9.98, height = 8.5, units = "in", dpi = 300)
+
+g_locreg_far_fBm_d1 <- gridExtra::grid.arrange(
+  ggplot_locreg(N = 400, lambda = 300, process = "FAR", white_noise = "fBm", design = "d1", param = "Ht"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FAR", white_noise = "fBm", design = "d1", param = "Ht_plus_mean"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FAR", white_noise = "fBm", design = "d1", param = "Lt"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FAR", white_noise = "fBm", design = "d1", param = "Lt_plus_mean"),
+  nrow = 2, ncol = 2
+)
+ggsave(filename = "./inst/12_mc_simulate_data/graphs/locreg_far_fBm_d1.png", plot = g_locreg_far_fBm_d1,
+       width = 9.98, height = 8.5, units = "in", dpi = 300)
+
+### FMA ----
+g_locreg_fma_mfBm_d1 <- gridExtra::grid.arrange(
+  ggplot_locreg(N = 400, lambda = 300, process = "FMA", white_noise = "mfBm", design = "d1", param = "Ht"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FMA", white_noise = "mfBm", design = "d1", param = "Ht_plus_mean"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FMA", white_noise = "mfBm", design = "d1", param = "Lt"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FMA", white_noise = "mfBm", design = "d1", param = "Lt_plus_mean"),
+  nrow = 2, ncol = 2
+)
+ggsave(filename = "./inst/12_mc_simulate_data/graphs/locreg_fma_mfBm_d1.png", plot = g_locreg_fma_mfBm_d1,
+       width = 9.98, height = 8.5, units = "in", dpi = 300)
+
+g_locreg_fma_fBm_d1 <- gridExtra::grid.arrange(
+  ggplot_locreg(N = 400, lambda = 300, process = "FMA", white_noise = "fBm", design = "d1", param = "Ht"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FMA", white_noise = "fBm", design = "d1", param = "Ht_plus_mean"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FMA", white_noise = "fBm", design = "d1", param = "Lt"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FMA", white_noise = "fBm", design = "d1", param = "Lt_plus_mean"),
+  nrow = 2, ncol = 2
+)
+ggsave(filename = "./inst/12_mc_simulate_data/graphs/locreg_fma_fBm_d1.png", plot = g_locreg_fma_fBm_d1,
+       width = 9.98, height = 8.5, units = "in", dpi = 300)
+
+## design 2 ----
+### FAR ----
+g_locreg_far_mfBm_d2 <- gridExtra::grid.arrange(
+  ggplot_locreg(N = 400, lambda = 300, process = "FAR", white_noise = "mfBm", design = "d2", param = "Ht"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FAR", white_noise = "mfBm", design = "d2", param = "Ht_plus_mean"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FAR", white_noise = "mfBm", design = "d2", param = "Lt"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FAR", white_noise = "mfBm", design = "d2", param = "Lt_plus_mean"),
+  nrow = 2, ncol = 2
+)
+ggsave(filename = "./inst/12_mc_simulate_data/graphs/locreg_far_mfBm_d2.png", plot = g_locreg_far_mfBm_d2,
+       width = 9.98, height = 8.5, units = "in", dpi = 300)
+
+g_locreg_far_fBm_d2 <- gridExtra::grid.arrange(
+  ggplot_locreg(N = 400, lambda = 300, process = "FAR", white_noise = "fBm", design = "d2", param = "Ht"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FAR", white_noise = "fBm", design = "d2", param = "Ht_plus_mean"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FAR", white_noise = "fBm", design = "d2", param = "Lt"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FAR", white_noise = "fBm", design = "d2", param = "Lt_plus_mean"),
+  nrow = 2, ncol = 2
+)
+ggsave(filename = "./inst/12_mc_simulate_data/graphs/locreg_far_fBm_d2.png", plot = g_locreg_far_fBm_d2,
+       width = 9.98, height = 8.5, units = "in", dpi = 300)
+
+### FMA ----
+g_locreg_fma_mfBm_d2 <- gridExtra::grid.arrange(
+  ggplot_locreg(N = 400, lambda = 300, process = "FMA", white_noise = "mfBm", design = "d2", param = "Ht"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FMA", white_noise = "mfBm", design = "d2", param = "Ht_plus_mean"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FMA", white_noise = "mfBm", design = "d2", param = "Lt"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FMA", white_noise = "mfBm", design = "d2", param = "Lt_plus_mean"),
+  nrow = 2, ncol = 2
+)
+ggsave(filename = "./inst/12_mc_simulate_data/graphs/locreg_fma_mfBm_d2.png", plot = g_locreg_fma_mfBm_d2,
+       width = 9.98, height = 8.5, units = "in", dpi = 300)
+
+g_locreg_fma_fBm_d2 <- gridExtra::grid.arrange(
+  ggplot_locreg(N = 400, lambda = 300, process = "FMA", white_noise = "fBm", design = "d2", param = "Ht"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FMA", white_noise = "fBm", design = "d2", param = "Ht_plus_mean"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FMA", white_noise = "fBm", design = "d2", param = "Lt"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FMA", white_noise = "fBm", design = "d2", param = "Lt_plus_mean"),
+  nrow = 2, ncol = 2
+)
+ggsave(filename = "./inst/12_mc_simulate_data/graphs/locreg_fma_fBm_d2.png", plot = g_locreg_fma_fBm_d2,
+       width = 9.98, height = 8.5, units = "in", dpi = 300)
+
+## design 3 ----
+### FAR ----
+g_locreg_far_mfBm_d3 <- gridExtra::grid.arrange(
+  ggplot_locreg(N = 400, lambda = 300, process = "FAR", white_noise = "mfBm", design = "d3", param = "Ht"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FAR", white_noise = "mfBm", design = "d3", param = "Ht_plus_mean"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FAR", white_noise = "mfBm", design = "d3", param = "Lt"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FAR", white_noise = "mfBm", design = "d3", param = "Lt_plus_mean"),
+  nrow = 2, ncol = 2
+)
+ggsave(filename = "./inst/12_mc_simulate_data/graphs/locreg_far_mfBm_d3.png", plot = g_locreg_far_mfBm_d3,
+       width = 9.98, height = 8.5, units = "in", dpi = 300)
+
+g_locreg_far_fBm_d3 <- gridExtra::grid.arrange(
+  ggplot_locreg(N = 400, lambda = 300, process = "FAR", white_noise = "fBm", design = "d3", param = "Ht"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FAR", white_noise = "fBm", design = "d3", param = "Ht_plus_mean"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FAR", white_noise = "fBm", design = "d3", param = "Lt"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FAR", white_noise = "fBm", design = "d3", param = "Lt_plus_mean"),
+  nrow = 2, ncol = 2
+)
+ggsave(filename = "./inst/12_mc_simulate_data/graphs/locreg_far_fBm_d3.png", plot = g_locreg_far_fBm_d3,
+       width = 9.98, height = 8.5, units = "in", dpi = 300)
+
+### FMA ----
+g_locreg_fma_mfBm_d3 <- gridExtra::grid.arrange(
+  ggplot_locreg(N = 400, lambda = 300, process = "FMA", white_noise = "mfBm", design = "d3", param = "Ht"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FMA", white_noise = "mfBm", design = "d3", param = "Ht_plus_mean"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FMA", white_noise = "mfBm", design = "d3", param = "Lt"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FMA", white_noise = "mfBm", design = "d3", param = "Lt_plus_mean"),
+  nrow = 2, ncol = 2
+)
+ggsave(filename = "./inst/12_mc_simulate_data/graphs/locreg_fma_mfBm_d3.png", plot = g_locreg_fma_mfBm_d3,
+       width = 9.98, height = 8.5, units = "in", dpi = 300)
+
+g_locreg_fma_fBm_d3 <- gridExtra::grid.arrange(
+  ggplot_locreg(N = 400, lambda = 300, process = "FMA", white_noise = "fBm", design = "d3", param = "Ht"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FMA", white_noise = "fBm", design = "d3", param = "Ht_plus_mean"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FMA", white_noise = "fBm", design = "d3", param = "Lt"),
+  ggplot_locreg(N = 400, lambda = 300, process = "FMA", white_noise = "fBm", design = "d3", param = "Lt_plus_mean"),
+  nrow = 2, ncol = 2
+)
+ggsave(filename = "./inst/12_mc_simulate_data/graphs/locreg_fma_fBm_d3.png", plot = g_locreg_fma_fBm_d3,
+       width = 9.98, height = 8.5, units = "in", dpi = 300)
+
+
+
